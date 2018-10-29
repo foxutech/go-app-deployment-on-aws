907951b55a6a44dd57080d9322b5ea019ac17cc6
FoxuTech <prabulugar@gmail.com>
2018-10-29 19:12:26 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {
+  description = "This name will prefix all resources, and be added as the value for the 'Name' tag where supported"
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  description = "This name will prefix all resources, and be added as the value for the 'Name' tag where supported"
+  type = "string"
+}
+
 variable "aws_region" {
   description = "AWS region on which we will setup the swarm cluster"
   default = "us-west-2"
