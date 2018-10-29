4ae004950f7034f9e8e95014e136d98312b2add1
FoxuTech <prabulugar@gmail.com>
2018-10-29 19:37:44 +0530

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -9,20 +9,12 @@ resource "aws_security_group" "instance" {
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 8082
+    to_port = 8082
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+ 
   egress {
     from_port = 0
     to_port = 0
