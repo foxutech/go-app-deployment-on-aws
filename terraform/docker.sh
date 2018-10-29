85ef99aecba192c505c39572e6fe85c2d46f7bd3
FoxuTech <prabulugar@gmail.com>
2018-10-29 23:29:38 +0530

    Update docker.sh

--- terraform/docker.sh
+++ terraform/docker.sh
@@ -3,8 +3,6 @@ apt update
 apt install git curl wget -y
 apt-add-repository ppa:ansible/ansible
 apt update
-apt install ansible -y
+apt install ansible python-pip -y
 curl -fsSL https://get.docker.com | sh
-git clone https://github.com/foxutech/go-app-deployment-on-aws.git
-cd go-app-deployment-on-aws
-ansible-playbook deployment.yml -vvvv -i localhost >> /home/ubuntu/ansible.txt
+pip install docker-py
